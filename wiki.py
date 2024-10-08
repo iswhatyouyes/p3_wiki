3eec25bd62bb4c9db02fca72d481732be7098229
iswhatyouyes g <07kalebg@gmail.com>
2024-10-08 17:55:03 +0000

    ruinedit

--- wiki.py
+++ wiki.py
@@ -14,14 +14,34 @@ def fetch_links(page):
     return links_list
 
 def wikipedia_game_solver(start_page, target_page):
-    links = fetch_links(start_page)
+    print("working on it.e..")
+    start_time = time.time()
+    
+    queue = Queue()
+    visited = set()
+    parent = {}
 
-    #make a loop that checks every item in links and prints out a message 
+    queue.put(start_page.title)
+    visited.add(start_page.title)
 
+print(start_page.links)
 
+    #reconstruct path from target page to start page / everything below this line is in the 
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title
+        path.append(page_title)
+        page_titile = parent[page_title]
+
+    path.append(start_page.title)
+    path.reverse()
+
+    end_time = time.time()
+    print("this algorithm took", end_time - start_time, "seconds.")
+    return path
 
 # start and end pages for our wikipedia game solver
 start_page = wiki.page("Pasadena High School (California)")
-target_page = wiki.page("Rose Parade")
-
-print(start_page.links)z
+target_page = wiki.page("Rose Parade")    
+path=wikipedi_game_solver(start_page,) target_page)
+print(path)
